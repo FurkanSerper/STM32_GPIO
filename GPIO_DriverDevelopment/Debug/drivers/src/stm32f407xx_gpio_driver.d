a49d956b15920ddc88daff5546491b017652f147
Furkan Serper <furkanserper0@gmail.com>
2025-02-22 16:47:16 +0000

    USART implementation is started

--- GPIO_DriverDevelopment/Debug/drivers/src/stm32f407xx_gpio_driver.d
+++ GPIO_DriverDevelopment/Debug/drivers/src/stm32f407xx_gpio_driver.d
@@ -4,9 +4,11 @@ drivers/src/stm32f407xx_gpio_driver.o: \
  C:/LowLevelDriverDevelopment/Workspace_LLDD/STM32_GPIO/GPIO_DriverDevelopment/drivers/inc/stm32f407xx.h \
  C:/LowLevelDriverDevelopment/Workspace_LLDD/STM32_GPIO/GPIO_DriverDevelopment/drivers/inc/stm32f407xx_gpio_driver.h \
  C:/LowLevelDriverDevelopment/Workspace_LLDD/STM32_GPIO/GPIO_DriverDevelopment/drivers/inc/stm32f407xx_spi_driver.h \
- C:/LowLevelDriverDevelopment/Workspace_LLDD/STM32_GPIO/GPIO_DriverDevelopment/drivers/inc/stm32f407xx_i2c_driver.h
+ C:/LowLevelDriverDevelopment/Workspace_LLDD/STM32_GPIO/GPIO_DriverDevelopment/drivers/inc/stm32f407xx_i2c_driver.h \
+ C:/LowLevelDriverDevelopment/Workspace_LLDD/STM32_GPIO/GPIO_DriverDevelopment/drivers/inc/stm32f407xx_usart_driver.h
 C:/LowLevelDriverDevelopment/Workspace_LLDD/STM32_GPIO/GPIO_DriverDevelopment/drivers/inc/stm32f407xx_gpio_driver.h:
 C:/LowLevelDriverDevelopment/Workspace_LLDD/STM32_GPIO/GPIO_DriverDevelopment/drivers/inc/stm32f407xx.h:
 C:/LowLevelDriverDevelopment/Workspace_LLDD/STM32_GPIO/GPIO_DriverDevelopment/drivers/inc/stm32f407xx_gpio_driver.h:
 C:/LowLevelDriverDevelopment/Workspace_LLDD/STM32_GPIO/GPIO_DriverDevelopment/drivers/inc/stm32f407xx_spi_driver.h:
 C:/LowLevelDriverDevelopment/Workspace_LLDD/STM32_GPIO/GPIO_DriverDevelopment/drivers/inc/stm32f407xx_i2c_driver.h:
+C:/LowLevelDriverDevelopment/Workspace_LLDD/STM32_GPIO/GPIO_DriverDevelopment/drivers/inc/stm32f407xx_usart_driver.h:
--- GPIO_DriverDevelopment/drivers/Inc/stm32f407xx.h
+++ GPIO_DriverDevelopment/drivers/Inc/stm32f407xx.h
@@ -140,6 +140,16 @@ typedef struct
 	__vo uint32_t FLTR;
 }I2C_RegDef_t;
 
+typedef struct
+{
+	__vo uint32_t SR;
+	__vo uint32_t DR;
+	__vo uint32_t BRR;
+	__vo uint32_t CR1;
+	__vo uint32_t CR2;
+	__vo uint32_t CR3;
+	__vo uint32_t GTPR;
+}USART_RegDef_t;
 
 
 
@@ -178,6 +188,7 @@ typedef struct
 	/* TODO : CKGATENR ?    DCKCFGR2 ? */
 
 }RCC_RegDef_t;
+
 
 
 
@@ -233,6 +244,15 @@ typedef struct
 #define I2C1			((I2C_RegDef_t*)I2C1_BASEADDR)
 #define I2C2			((I2C_RegDef_t*)I2C2_BASEADDR)
 #define I2C3			((I2C_RegDef_t*)I2C3_BASEADDR)
+
+#define USART1			((USART_RegDef_t*)USART1_BASEADDR)
+#define USART2			((USART_RegDef_t*)USART2_BASEADDR)
+#define USART3			((USART_RegDef_t*)USART3_BASEADDR)
+#define USART6			((USART_RegDef_t*)USART6_BASEADDR)
+
+
+
+
 
 
 /*
@@ -521,10 +541,92 @@ typedef struct
 #define I2C_CCR_FS			15
 
 
+/*
+ * USART SR Register
+ */
+#define USART_SR_PE			0
+#define USART_SR_FE			1
+#define USART_SR_NF			2
+#define USART_SR_ORE		3
+#define USART_SR_IDLE		4
+#define USART_SR_RXNE		5
+#define USART_SR_TC			6
+#define USART_SR_TXE		7
+#define USART_SR_LBD		8
+#define USART_SR_CTS		9
 
 
+/*
+ * USART DR Register
+ */
+#define USART_DR_DR			0
+
+
+/*
+ * USART BRR Register
+ */
+#define USART_BRR_DIV_Fraction 		0
+#define USART_BRR_DIV_Mantissa		4
+
+
+/*
+ * USART CR1 Register
+ */
+#define USART_CR1_SBK				0
+#define USART_CR1_RWU				1
+#define USART_CR1_RE				2
+#define USART_CR1_TE				3
+#define USART_CR1_IDLEIE			4
+#define USART_CR1_RXNEIE			5
+#define USART_CR1_TCIE				6
+#define USART_CR1_TXEIE				7
+#define USART_CR1_PEIE				8
+#define USART_CR1_PS				9
+#define USART_CR1_PCE				10
+#define USART_CR1_WAKE				11
+#define USART_CR1_M					12
+#define USART_CR1_UE				13
+#define USART_CR1_OVER8				15
+
+/*
+ * USART CR2 Register
+ */
+#define USART_CR2_ADD				0
+#define USART_CR2_LBDL				5
+#define USART_CR2_LBDIE				6
+#define USART_CR2_LBCL				8
+#define USART_CR2_CPHA				9
+#define USART_CR2_CPOL				10
+#define USART_CR2_CLKEN				11
+#define USART_CR2_STOP				12
+#define USART_CR2_LIKEN				14
+
+
+/*
+ * USART CR3 Register
+ */
+#define USART_CR3_EIE				0
+#define USART_CR3_IREN				1
+#define USART_CR3_IRLP				2
+#define USART_CR3_HDSEL				3
+#define USART_CR3_NACK				4
+#define USART_CR3_SCEN				5
+#define USART_CR3_DMAR				6
+#define USART_CR3_DMAT				7
+#define USART_CR3_RTSE				8
+#define USART_CR3_CTSE				9
+#define USART_CR3_CTSIE				10
+#define USART_CR3_ONEBIT			11
+
+
+/*
+ * USART GTPR Register
+ */
+#define USART_GTPR_PSC				0
+#define USART_GTPR_GT				8
 
 #include "stm32f407xx_gpio_driver.h"
 #include "stm32f407xx_spi_driver.h"
 #include "stm32f407xx_i2c_driver.h"
+#include "stm32f407xx_usart_driver.h"
 #endif /* INC_STM32F407XX_H_ */
